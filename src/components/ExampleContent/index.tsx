cd4511ce4a5a0ada2794205407732b045c7a73af
mdawlad29 <mdawladbdinfo@gmail.com>
2025-01-07 00:26:05 +0600

    sass implement

---
+++ src/components/ExampleContent/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExampleContent = () => {
+  return (
+    <div className="text-lg">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iste
+      possimus, consectetur quaerat quia natus ratione iusto enim odit magni
+      nihil? Id fugiat porro, esse ex soluta distinctio provident maxime.
+    </div>
+  );
+};
+
+export default ExampleContent;
